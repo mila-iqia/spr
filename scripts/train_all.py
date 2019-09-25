15f25b274b72bd11f94f095f4da496b26deb4e5e
ankeshanand <ankeshanand1994@gmail.com>
2019-09-25 16:29:37 -0400

    Remove redundant logging

--- scripts/train_all.py
+++ scripts/train_all.py
@@ -55,7 +55,6 @@ def train_policy(args):
                                       log_last_only=True)
             else:
                 forward_model.train(real_transitions,
-                                    init_epoch=j,
                                     log_last_only=True)
 
         steps = j * args.env_steps_per_epoch
@@ -108,15 +107,8 @@ def train_policy(args):
 
             if j >= 1:
                 # Update policy parameters on model data
-                avg_loss, avg_weighted_loss = 0., 0.
                 for g in range(args.updates_per_step):
-                    loss, weighted_loss = dqn.learn(model_transitions)
-                    avg_loss += loss
-                    avg_weighted_loss += weighted_loss
-
-                avg_loss /= args.updates_per_step
-                avg_weighted_loss /= args.updates_per_step
-                wandb.log({'Q-Loss': avg_loss, 'Weighted Q-Loss': avg_weighted_loss})
+                    dqn.learn(model_transitions)
 
         if j > 0:
             dqn.log(iter=j)
