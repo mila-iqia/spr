575619084b819a1b43760ea08f482a906cd1ba94
Max Schwarzer <MaxA.Schwarzer@gmail.com>
2019-09-30 16:19:08 -0400

    Add extra capacity to prediction/reward modules

--- src/stdim_with_actions.py
+++ src/stdim_with_actions.py
@@ -29,6 +29,8 @@ class PredictionModule(nn.Module):
         self.network = nn.Sequential(
             nn.Linear(state_dim*num_actions, state_dim*4),
             nn.ReLU(),
+            nn.Linear(state_dim*4, state_dim*4),
+            nn.ReLU(),
             nn.Linear(state_dim*4, state_dim))
 
     def forward(self, states, actions):
@@ -43,6 +45,8 @@ class RewardPredictionModule(nn.Module):
         super().__init__()
         self.network = nn.Sequential(
             nn.Linear(state_dim*num_actions, state_dim*4),
+            nn.ReLU(),
+            nn.Linear(state_dim*4, state_dim*4),
             nn.ReLU(),
             nn.Linear(state_dim*4, reward_dim))
 
