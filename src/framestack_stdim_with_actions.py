d81836edb3804ccf9a0828d90a09bee7b39f108f
Max Schwarzer <MaxA.Schwarzer@gmail.com>
2019-10-02 15:05:09 -0400

    Add option for global bilinear loss

--- src/framestack_stdim_with_actions.py
+++ src/framestack_stdim_with_actions.py
@@ -59,7 +59,7 @@ class RewardPredictionModule(nn.Module):
 
 
 class FramestackActionInfoNCESpatioTemporalTrainer(Trainer):
-    def __init__(self, encoder, config, device=torch.device('cpu'), wandb=None):
+    def __init__(self, encoder, config, device=torch.device('cpu'), wandb=None, agent=None):
         super().__init__(encoder, wandb, device)
         self.config = config
         self.patience = self.config["patience"]
@@ -68,13 +68,17 @@ class FramestackActionInfoNCESpatioTemporalTrainer(Trainer):
         self.epochs = config['epochs']
         self.batch_size = config['batch_size']
         self.global_loss = config['global_loss']
+        self.bilinear_global_loss = config['bilinear_global_loss']
         self.noncontrastive_global_loss = config['noncontrastive_global_loss']
         self.noncontrastive_loss_weight = config['noncontrastive_loss_weight']
 
         self.device = device
         self.classifier = nn.Linear(self.encoder.hidden_size, 64).to(device)
+        self.global_classifier = nn.Linear(self.encoder.hidden_size,
+                                           self.encoder.hidden_size).to(device)
         self.params = list(self.encoder.parameters())
         self.params += list(self.classifier.parameters())
+        self.params += list(self.global_classifier.parameters())
 
         self.prediction_module = PredictionModule(self.encoder.hidden_size,
                                                   config["num_actions"])
@@ -101,7 +105,7 @@ class FramestackActionInfoNCESpatioTemporalTrainer(Trainer):
         # Sample `num_samples` episodes then batchify them with `self.batch_size` episodes per batch
         for idx in range(total_steps // self.batch_size):
             indices = np.random.randint(0, total_steps-1, size=self.batch_size)
-            x_t, x_tnext, a_t, r_tnext = [], [], [], []
+            x_t, x_tnext, a_t, r_tnext, dones = [], [], [], [], []
             for t in indices:
                 # Get one sample from this episode
                 while transitions[t].nonterminal is False:
@@ -120,11 +124,13 @@ class FramestackActionInfoNCESpatioTemporalTrainer(Trainer):
                 x_tnext.append(transitions[t+1].state)
                 a_t.append(transitions[t].action)
                 r_tnext.append(transitions[t+1].reward + 1)
+                dones.append(transitions[t+1].nonterminal)
 
             yield torch.stack(x_t).to(self.device).float() / 255., \
                   torch.stack(x_tnext).to(self.device).float() / 255., \
                   torch.tensor(a_t, device=self.device).long(), \
-                  torch.tensor(r_tnext, device=self.device).long()
+                  torch.tensor(r_tnext, device=self.device).long(), \
+                  torch.tensor(dones, device=self.device).unsqueeze(-1).float()
 
     def generate_reward_class_weights(self, transitions):
         counts = [0, 0, 0]  # counts for reward=-1,0,1
@@ -166,17 +172,18 @@ class FramestackActionInfoNCESpatioTemporalTrainer(Trainer):
         representation_norm = 0
 
         data_generator = self.generate_batch(episodes)
-        for x_tprev, x_t, actions, rewards in data_generator:
+        for x_tprev, x_t, actions, rewards, done in data_generator:
             x_tprev = x_tprev.view(x_t.shape[0]*4, *x_tprev.shape[2:])
             f_t_maps, f_t_prev_maps = self.encoder(x_t, fmaps=True),\
                                       self.encoder(x_tprev, fmaps=True)
 
-            f_t_initial = f_t_prev_maps["out"].view(x_t.shape[0], 4, -1)[:, -1]
+            f_t_prev_stack = f_t_prev_maps["out"].view(x_t.shape[0], 4, -1)
+            f_t_initial = f_t_prev_stack[:, -1]
             f_t_prev = f_t_prev_maps["out"].view(x_t.shape[0], -1)
-
-            # Loss 1: Global at time t, f5 patches at time t-1
             f_t = f_t_maps['f5']
             f_t_global = f_t_maps["out"]
+
+            # Loss 1: Global at time t, f5 patches at time t-1
             sy = f_t.size(1)
             sx = f_t.size(2)
             actions = self.convert_actions(actions).float()
@@ -227,6 +234,12 @@ class FramestackActionInfoNCESpatioTemporalTrainer(Trainer):
             if self.global_loss:
                 diff = f_t_pred.unsqueeze(0) - f_t_global.unsqueeze(1)
                 logits = -torch.norm(diff, p=2, dim=-1)
+                loss2 = F.cross_entropy(logits, torch.arange(N).to(self.device))
+                epoch_global_loss += loss2.detach().item()
+            elif self.bilinear_global_loss:
+                f_t_pred = self.global_classifier(f_t_pred)
+                f_t_pred_delta = f_t_pred - f_t_initial
+                logits = torch.matmul(f_t_pred, f_t_global.t())
                 loss2 = F.cross_entropy(logits, torch.arange(N).to(self.device))
                 epoch_global_loss += loss2.detach().item()
             else:
@@ -297,8 +310,10 @@ class FramestackActionInfoNCESpatioTemporalTrainer(Trainer):
         N = z.size(0)
         z_last = z.view(N, 4, -1)[:, -1, :]  # choose the last latent vector from z
         z = z.view(N, -1)
-        new_states = self.prediction_module(z, a)
-        return new_states + z_last, self.reward_module(z, a).argmax(-1) - 1
+        new_states = self.prediction_module(z, a) + z_last
+        if self.bilinear_global_loss:
+            new_states = self.global_classifier(new_states)
+        return new_states, self.reward_module(z, a).argmax(-1) - 1
 
     def log_results(self,
                     local_loss,
@@ -314,7 +329,7 @@ class FramestackActionInfoNCESpatioTemporalTrainer(Trainer):
                     zero_precision,
                     prefix=""):
         print(
-            "{} Epoch: {}, Epoch Loss: {:.3f}, Local Loss: {:.3f}, Reward Loss: {:.3f}, Global Loss: {:.3f}, Dynamics Error: {:.3f}, Prediction Cosine Similarity: {:.3f}, Reward Accuracy: {:.3f} {}".format(
+            "{} Epoch: {}, Epoch Loss: {:.3f}, Local Loss: {:.3f}, Reward Loss: {:.3f}, Global Loss: {:.3f}, Dynamics Error: {:.3f}, Prediction Cosine Similarity: {:.3f}, Reward Accuracy: {:.3f}, {}".format(
                 prefix.capitalize(),
                 self.epochs_till_now,
                 epoch_loss,
