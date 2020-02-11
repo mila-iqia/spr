Fix bugs in normalization, and an off-by-one error in backup, bug that caused all obs to be same, better inits for policy and value network (#19)
* Fix bugs in normalization, and an off-by-one error in backup

* Fix a nasty bug which caused all obs to be the same, use int obs everywhere except model training, other minor fixes, 

* Minor fixes

* Minor fixes

scripts/run_pizero.py
@@ -47,13 +47,12 @@ def run_pizero(args):
                           asynchronous=not args.sync_envs)
     # TODO return int observations
     obs = env.reset()
+    obs = torch.from_numpy(obs)
     vectorized_mcts = VectorizedMCTS(args, env.action_space[0].n, args.num_envs, target_network)
     eval_vectorized_mcts = VectorizedMCTS(args, env.action_space[0].n, args.evaluation_episodes, target_network, eval=True)
     total_episodes = 0.
     total_train_steps = 0
     while env_steps < args.total_env_steps:
-        obs = torch.from_numpy(obs)
-
         # Run MCTS for the vectorized observation
         actions, policies, values = vectorized_mcts.run(obs)
         policy_probs = policies.probs
@@ -73,7 +72,7 @@ def run_pizero(args):
 
         if args.reanalyze:
             async_reanalyze.store_transitions(
-                (obs[:, -1]*255).byte(),
+                obs[:, -1],
                 actions,
                 reward,
                 done,
@@ -125,13 +124,12 @@ def run_pizero(args):
             wandb.log({'Mean Reward': np.mean(episode_rewards), 'Median Reward': np.median(episode_rewards),
                        'env_steps': env_steps})
 
-        if env_steps % args.evaluation_interval == 0 and env_steps > 0:
-            print('Evaluating')
+        if env_steps % args.evaluation_interval == 0:
             avg_reward = eval_vectorized_mcts.evaluate()
             print('Env steps: {}, Avg_Reward: {}'.format(env_steps, avg_reward))
             wandb.log({'env_steps': env_steps, 'avg_reward': avg_reward})
 
-        obs = next_obs
+        obs.copy_(torch.from_numpy(next_obs))
         env_steps += args.num_envs
 
 

src/envs.py
@@ -30,7 +30,7 @@ class AtariEnv(gym.core.Env):
         self.training = True  # Consistent with model training mode
         self.grayscale = args.grayscale
         channels = 1 if self.grayscale else 3
-        self.observation_space = gym.spaces.Box(low=-float('inf'), high=float('inf'), shape=(args.framestack, channels, 96, 96))
+        self.observation_space = gym.spaces.Box(low=0, high=255, dtype=np.uint8, shape=(args.framestack, channels, 96, 96))
         self.action_space = gym.spaces.Discrete(len(self.actions))
 
     def _get_state(self):
@@ -38,16 +38,16 @@ class AtariEnv(gym.core.Env):
             obs = self.ale.getScreenGrayscale()
         else:
             obs = self.ale.getScreenRGB()
-        state = cv2.resize(obs, (96, 96), interpolation=cv2.INTER_LINEAR)/255.
+        state = cv2.resize(obs, (96, 96), interpolation=cv2.INTER_NEAREST)
         if len(state.shape) == 2:
             state = state[..., None]
         return state
 
     def _reset_buffer(self):
         if self.grayscale:
-            blank = np.zeros((96, 96, 1))
+            blank = np.zeros((96, 96, 1), dtype=np.uint8)
         else:
-            blank = np.zeros((96, 96, 3))
+            blank = np.zeros((96, 96, 3), dtype=np.uint8)
         for _ in range(self.window):
             self.state_buffer.append(blank)
 
@@ -76,7 +76,7 @@ class AtariEnv(gym.core.Env):
             channels = 1
         else:
             channels = 3
-        frame_buffer = np.zeros((2, 96, 96, channels))
+        frame_buffer = np.zeros((2, 96, 96, channels), dtype=np.uint8)
         reward, done = 0, False
         for t in range(4):
             reward += self.ale.act(self.actions.get(action))