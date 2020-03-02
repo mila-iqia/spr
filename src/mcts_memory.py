Add episode length logging; fix issue with dones in initial priority calculation

src/mcts_memory.py
@@ -57,7 +57,7 @@ def initialize_replay_buffer(args):
     )
 
     if args.prioritized:
-        replay_kwargs["input_priorities"]=args.input_priorities,
+        replay_kwargs["input_priorities"] = args.input_priorities,
         buffer = AsyncPrioritizedSequenceReplayFrameBufferExtended(**replay_kwargs)
     else:
         buffer = AsyncUniformSequenceReplayFrameBufferExtended(**replay_kwargs)
@@ -113,7 +113,7 @@ class AsyncUniformSequenceReplayFrameBufferExtended(AsyncUniformSequenceReplayFr
             batch.all_action[ind+1:, i] = batch.all_action[ind, i]
             batch.policy_probs[ind+1:, i] = batch.policy_probs[ind, i]
             batch.all_reward[ind+1:, i] = 0
-            batch.values[ind+1:, i] = 0
+            batch.values[ind:, i] = 0
         return batch
 
 
@@ -192,19 +192,21 @@ class LocalBuffer:
         self.values.append(vec_v)
         self.value_estimates.append(vec_v_est)
 
-    def calculate_initial_priorities(self, rewards, values, value_estimates):
+    def calculate_initial_priorities(self, rewards, values, value_estimates, dones):
         """
         :param rewards: Stacked rewards for the buffer.
         :param value_targets: Stacked value targets for the buffer.
         :param value_estimates: Stacked initial value estimates for the buffer.
         :return: Value errors.
         """
-        discounts = torch.ones_like(rewards)[:self.args.multistep]*self.args.discount
-        discounts = discounts ** torch.arange(0, self.args.multistep)[:, None].float()
+        done_mask = torch.cumprod(1 - dones.float(), 0)
+        discounts = torch.ones_like(rewards)[:self.args.multistep+1]*self.args.discount
+        discounts = discounts ** torch.arange(0, self.args.multistep+1)[:, None].float()
+        discounts = discounts * done_mask[:-self.args.multistep-1]
 
         valid_range = rewards[:-self.args.multistep].shape[0]
-        discounted_rewards = torch.cat([rewards[i:i+self.args.multistep]*discounts for i in range(valid_range)], 0)
-        value_targets = values[self.args.multistep:]*(self.args.discount ** self.args.multistep)
+        discounted_rewards = torch.stack([torch.sum(rewards[i:i+self.args.multistep]*discounts, 0) for i in range(valid_range)], 0)
+        value_targets = values[self.args.multistep:]*discounts[-1]
 
         value_targets = discounted_rewards + value_targets
 
@@ -219,7 +221,8 @@ class LocalBuffer:
         if self.args.input_priorities:
             priorities = self.calculate_initial_priorities(samples[2],
                                                            samples[5],
-                                                           torch.stack(self.value_estimates))
+                                                           torch.stack(self.value_estimates),
+                                                           samples[3])
             samples = [t[:-self.args.multistep] for t in samples]
             samples.append(priorities)
 

src/vectorized_mcts.py
@@ -202,7 +202,6 @@ class VectorizedMCTS:
             # Calculate the return as observed by the new parent.
             returns = returns*self.args.discount + reward
 
-
             # Update q and count at the parent for the actions taken then
             self.visit_count[self.batch_range, parent_id.squeeze(), actions.squeeze()] += not_done_mask.squeeze()
             self.virtual_loss[self.batch_range, parent_id.squeeze(), actions.squeeze()] += (self.vl_c * not_done_mask.squeeze())
@@ -276,6 +275,10 @@ class VectorizedMCTS:
 
 
 class VectorizedQMCTS(VectorizedMCTS):
+    # def __init__(self, *args, **kwargs):
+    #     super(VectorizedQMCTS, self).__init__(*args, **kwargs)
+    #     self.root_exploration_fraction = 0.05
+
     def reset_tensors(self):
         super().reset_tensors()
         self.visit_count.fill_(1)
@@ -290,8 +293,8 @@ class VectorizedQMCTS(VectorizedMCTS):
         self.q[:, 0] = initial_value.to(self.search_device)
         self.min_q = torch.min(self.q[:, 0], dim=-1)[0]
         self.max_q = torch.max(self.q[:, 0], dim=-1)[0]
-        # self.prior[:, 0] = F.softmax(policy_logits, dim=-1)
-        # self.add_exploration_noise()
+        if self.args.q_dirichlet:
+            self.add_exploration_noise()
 
         for sim_id in range(1, self.n_sims+1):
             # Pre-compute action to select at each node in case it is visited in this sim
@@ -369,6 +372,18 @@ class VectorizedQMCTS(VectorizedMCTS):
         values /= (self.max_q - self.min_q)[:, None, None]
         values = valid_indices * values
         return values
+
+    def add_exploration_noise(self):
+        concentrations = torch.tensor([self.root_dirichlet_alpha] * self.num_actions, device=self.search_device)
+        noise = torch.distributions.dirichlet.Dirichlet(concentrations).sample((self.n_runs,))
+        frac = self.root_exploration_fraction
+
+        q_dist = F.softmax(self.q[:, 0], -1)
+        mixed_dist = (q_dist * (1-frac)) + (noise * frac)
+        est_q = torch.log(mixed_dist)
+        mean_offset = self.q[:, 0].mean(-1, keepdim=True) - est_q.mean(-1, keepdim=True)
+
+        self.q[:, 0] = est_q + mean_offset
 
     def backup(self, id_final, depth, value_final):
         returns = value_final.max(dim=-1)[0]