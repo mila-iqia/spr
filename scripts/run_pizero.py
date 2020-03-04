Fix input priority done masking bug

scripts/run_pizero.py
@@ -123,6 +123,7 @@ def run_pizero(args):
             obs, actions, reward, done, policies, values, value_estimates = obs.cpu(), actions.cpu(), reward.cpu(),\
                                                                             done.cpu(), policies.cpu(),\
                                                                             values.cpu(), value_estimates.cpu()
+
             eprets += np.array(reward)
             eplens += 1
             for i in range(args.num_envs):
@@ -160,7 +161,7 @@ def run_pizero(args):
             else:
                 local_buf.append(obs, actions, reward, done, policies, values, value_estimates)
 
-            if env_steps % (args.jumps + args.multistep) == 0 and env_steps > 0:
+            if env_steps//args.num_envs % (args.jumps + args.multistep + 1) == 0 and env_steps > args.num_envs*50:
                 # Send transitions from the local buffer to the replay buffer
                 buffer.append_samples(samples_to_buffer(*local_buf.stack()))
                 local_buf.clear()

src/mcts_memory.py
@@ -227,18 +227,19 @@ class LocalBuffer:
         :param value_estimates: Stacked initial value estimates for the buffer.
         :return: Value errors.
         """
-        done_mask = torch.cumprod(1 - dones.float(), 0)
+        valid_range = rewards[:-self.args.multistep].shape[0]
+        dones = 1 - dones.float()
+        done_masks = torch.stack([torch.cumprod(dones[i:i+self.args.multistep+1], 0) for i in range(valid_range)])
         discounts = torch.ones_like(rewards)[:self.args.multistep+1]*self.args.discount
         discounts = discounts ** torch.arange(0, self.args.multistep+1)[:, None].float()
-        discounts = discounts * done_mask[:-self.args.multistep-1]
+        discounts = discounts
 
-        valid_range = rewards[:-self.args.multistep].shape[0]
-        discounted_rewards = torch.stack([torch.sum(rewards[i:i+self.args.multistep]*discounts, 0) for i in range(valid_range)], 0)
-        value_targets = values[self.args.multistep:]*discounts[-1]
+        discounted_rewards = torch.stack([torch.sum(discounts[:-1]*done_masks[i, :-1]*rewards[i:i+self.args.multistep], 0) for i in range(valid_range)], 0)
+        value_targets = values[self.args.multistep:]*discounts[-1]*done_masks[:, -1]
 
         value_targets = discounted_rewards + value_targets
 
-        errors = torch.abs(value_estimates[:self.args.multistep] - value_targets) + 0.001
+        errors = torch.abs(value_estimates[self.args.multistep:] - value_targets) + 0.001
 
         return errors
 