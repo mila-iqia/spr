4a407e8143d24fec4a6283ad1f0c5e4ab1aa3003
ankeshanand <ankeshanand1994@gmail.com>
2020-03-25 05:12:48 -0400

    Add dueling head for RepNet

--- src/rlpyt_models.py
+++ src/rlpyt_models.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 
 from rlpyt.models.dqn.atari_catdqn_model import DistributionalHeadModel
 from rlpyt.models.dqn.dueling import DistributionalDuelingHeadModel
+from rlpyt.models.utils import scale_grad
 from rlpyt.runners.async_rl import AsyncRlEval
 from rlpyt.runners.minibatch_rl import MinibatchRlEval
 from rlpyt.utils.tensor import infer_leading_dims, restore_leading_dims
@@ -102,7 +103,10 @@ class PizeroCatDqnModel(torch.nn.Module):
         # conv_out_size = self.conv.conv_out_size(h, w)
         # self.dyamics_network = TransitionModel(conv_out_size, num_actions)
         # self.reward_network = ValueNetwork(conv_out_size)
-        self.head = PizeroDistributionalHeadModel(256, output_size)
+        if dueling:
+            self.head = PizeroDistributionalDuelingHeadModel(256, output_size)
+        else:
+            self.head = PizeroDistributionalHeadModel(256, output_size)
 
     def forward(self, observation, prev_action, prev_reward):
         """Returns the probability masses ``num_atoms x num_actions`` for the Q-values
@@ -141,4 +145,43 @@ class PizeroDistributionalHeadModel(torch.nn.Module):
 
     def forward(self, input):
         return self.network(input).view(-1, self._output_size, self._n_atoms)
+
+
+class PizeroDistributionalDuelingHeadModel(torch.nn.Module):
+    """An MLP head which reshapes output to [B, output_size, n_atoms]."""
+
+    def __init__(self, input_channels, output_size, hidden_size=128, pixels=30, n_atoms=51, grad_scale=2 ** (-1 / 2)):
+        super().__init__()
+        self.hidden_size = hidden_size
+        layers = [nn.Conv2d(input_channels, hidden_size, kernel_size=1, stride=1),
+                  nn.ReLU(),
+                  nn.BatchNorm2d(hidden_size),
+                  nn.Flatten(-3, -1),
+                  nn.Linear(pixels*hidden_size, 512),
+                  nn.ReLU(),
+                  nn.Linear(512, output_size*n_atoms)]
+        self.advantage_hidden = nn.Sequential(
+            nn.Conv2d(input_channels, hidden_size, kernel_size=1, stride=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(hidden_size),
+            nn.Flatten(-3, -1))
+        self.advantage_out = torch.nn.Linear(pixels*hidden_size,
+                                             output_size * n_atoms, bias=False)
+        self.advantage_bias = torch.nn.Parameter(torch.zeros(n_atoms))
+        self.value = nn.Sequential(*layers)
+        self._grad_scale = grad_scale
+        self._output_size = output_size
+        self._n_atoms = n_atoms
+
+    def forward(self, input):
+        x = scale_grad(input, self._grad_scale)
+        advantage = self.advantage(x)
+        value = self.value(x).view(-1, 1, self._n_atoms)
+        return value + (advantage - advantage.mean(dim=1, keepdim=True))
+
+    def advantage(self, input):
+        x = self.advantage_hidden(input)
+        x = self.advantage_out(x)
+        x = x.view(-1, self._output_size, self._n_atoms)
+        return x + self.advantage_bias
 
