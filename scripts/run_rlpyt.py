f5826a6d8ef7d0b497a819a55526454140cd983d
Max Schwarzer <MaxA.Schwarzer@gmail.com>
2020-04-21 15:02:58 -0700

    Add in affine transformations, moco-style NCE

--- scripts/run_rlpyt.py
+++ scripts/run_rlpyt.py
@@ -41,12 +41,13 @@ def debug_build_and_train(game="pong", run_ID=0, cuda_idx=0, model=False, detach
     config["eval_env"]["stack_actions"] = args.stack_actions
     config["eval_env"]["grayscale"] = args.grayscale
     config["algo"]["n_step_return"] = 5
-    config["algo"]["replay_size"] = 1e5
     config["algo"]["prioritized_replay"] = True
     config["algo"]["min_steps_learn"] = 1e3
+    config["algo"]["n_step_return"] = 20
+    config["algo"]["batch_size"] = 64
+    config["algo"]["learning_rate"] = 0.0001
     config["sampler"]["eval_max_trajectories"] = 5
     config["sampler"]["eval_n_envs"] = 5
-    config["sampler"]["batch_B"] = 128
     wandb.config.update(config)
     sampler = SerialSampler(
         EnvCls=AtariEnv,
@@ -54,8 +55,8 @@ def debug_build_and_train(game="pong", run_ID=0, cuda_idx=0, model=False, detach
         env_kwargs=config["env"],
         eval_env_kwargs=dict(game=game),
         batch_T=4,  # Four time-steps per sampler iteration.
-        batch_B=16,
-        max_decorrelation_steps=0,
+        batch_B=4,
+        max_decorrelation_steps=1000,
     )
     args.discount = config["algo"]["discount"]
     if model:
@@ -65,6 +66,7 @@ def debug_build_and_train(game="pong", run_ID=0, cuda_idx=0, model=False, detach
         config["model"]["film"] = args.film
         config["model"]["norm_type"] = args.norm_type
         config["model"]["nce"] = args.nce
+        config["model"]["nce_type"] = args.nce_type
         config["model"]["augmentation"] = args.augmentation
         config["model"]["stack_actions"] = args.stack_actions
         algo = PizeroModelCategoricalDQN(optim_kwargs=config["optim"], jumps=args.jumps, **config["algo"], detach_model=detach_model)  # Run with defaults.
@@ -138,6 +140,7 @@ def build_and_train(game="ms_pacman", run_ID=0, model=False, detach_model=1, arg
         config["model"]["dynamics_blocks"] = args.dynamics_blocks
         config["model"]["film"] = args.film
         config["model"]["nce"] = args.nce
+        config["model"]["nce_type"] = args.nce_type
         config["model"]["norm_type"] = args.norm_type
         config["model"]["augmentation"] = args.augmentation
         config["model"]["stack_actions"] = args.stack_actions
@@ -194,10 +197,11 @@ if __name__ == "__main__":
     parser.add_argument('--beluga', action="store_true")
     parser.add_argument('--jumps', type=int, default=4)
     parser.add_argument('--dynamics-blocks', type=int, default=16)
-    parser.add_argument('--norm-type', type=str, default='bn', choices=["bn", "ln", "in", "none"], help='Optimizer')
+    parser.add_argument('--norm-type', type=str, default='bn', choices=["bn", "ln", "in", "none"], help='Normalization')
     parser.add_argument('--film', type=int, default=0)
     parser.add_argument('--nce', type=int, default=0)
-    parser.add_argument('--augmentation', type=int, default=0)
+    parser.add_argument('--nce-type', type=str, default='stdim', choices=["stdim", "moco"], help='Style of NCE')
+    parser.add_argument('--augmentation', type=str, default='none', choices=["none", "affine", "crop"], help='Style of augmentation')
     parser.add_argument('--detach-model', type=int, default=1)
     parser.add_argument('--debug_cuda_idx', help='gpu to use ', type=int, default=0)
     # MCTS arguments
