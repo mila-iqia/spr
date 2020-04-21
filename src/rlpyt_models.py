9faf3b8665a93eef2dda5aa8b61a41d26784ff57
Max Schwarzer <MaxA.Schwarzer@gmail.com>
2020-04-21 15:33:15 -0700

    Remove unused code

--- src/rlpyt_models.py
+++ src/rlpyt_models.py
@@ -597,24 +597,3 @@ class RepNet(nn.Module):
             x = x.unsqueeze(0)
         latent = self.network(x)
         return renormalize(latent, 1)
-
-def curl_random_crop(imgs, out):
-    """
-    Vectorized random crop
-    args:
-    imgs: shape (B,C,H,W)
-    out: output size (e.g. 84)
-    """
-    # n: batch size.
-    n = imgs.shape[0]
-    img_size = imgs.shape[-1] # e.g. 100
-    crop_max = img_size - out
-    imgs = np.transpose(imgs, (0, 2, 3, 1))
-    w1 = np.random.randint(0, crop_max, n)
-    h1 = np.random.randint(0, crop_max, n)
-    # creates all sliding window
-    # combinations of size (out)
-    windows = view_as_windows(imgs, (1, out, out, 1))[..., 0,:,:, 0]
-    # selects a random window# for each batch element
-    cropped = windows[np.arange(n), w1, h1]
-    return cropped
