12cd436ccd0530f07937ac73decf5ff8d1d43655
ankeshanand <ankeshanand1994@gmail.com>
2020-04-26 20:55:10 -0400

    Change std-init of noisy layers to 0.1 to match DERainbow

--- src/rlpyt_models.py
+++ src/rlpyt_models.py
@@ -754,7 +754,7 @@ class CurlEncoder(nn.Module):
 
 
 class NoisyLinear(nn.Module):
-    def __init__(self, in_features, out_features, std_init=0.5):
+    def __init__(self, in_features, out_features, std_init=0.1):
         super(NoisyLinear, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
