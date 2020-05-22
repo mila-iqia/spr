f066b7917c2917f7330edcedddc3e2279f96d8bd
Max Schwarzer <MaxA.Schwarzer@gmail.com>
2020-05-21 20:34:16 -0700

    Add frame dropout, cosine nce option

--- src/utils.py
+++ src/utils.py
@@ -248,3 +248,14 @@ def find_free_port():
     sock.close()
     return sockname[1]
 
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+class dummy_context_mgr:
+    def __enter__(self):
+        return None
+    def __exit__(self, exc_type, exc_value, traceback):
+        return False
+
