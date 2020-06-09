50e05c3a5cfe8910559b69428e60b82b97dc7c1f
Max Schwarzer <MaxA.Schwarzer@gmail.com>
2020-06-09 10:30:22 -0700

    Fix num eval trajs

--- scripts/run_rlpyt.py
+++ scripts/run_rlpyt.py
@@ -63,8 +63,8 @@ def debug_build_and_train(game="pong", run_ID=0, cuda_idx=0, model=False, detach
     config['algo']['pri_alpha'] = 0.5
     config['algo']['pri_beta_steps'] = int(10e4)
     config['optim']['eps'] = 0.00015
-    config["sampler"]["eval_max_trajectories"] = 10
-    config["sampler"]["eval_n_envs"] = 10
+    config["sampler"]["eval_max_trajectories"] = 100
+    config["sampler"]["eval_n_envs"] = 100
     config["sampler"]["eval_max_steps"] = 100*28000
     config['sampler']['batch_B'] = args.batch_b
     config['sampler']['batch_T'] = args.batch_t
