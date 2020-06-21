bf92aebfb5727360419521abd4b1c56e789e92d9
ankeshanand <ankeshanand1994@gmail.com>
2020-06-21 18:09:39 -0400

    Handle life loss / game overs in b/w frameskips.

--- src/rlpyt_atari_env.py
+++ src/rlpyt_atari_env.py
@@ -134,17 +134,20 @@ class AtariEnv(Env):
     def step(self, action):
         a = self._action_set[action]
         game_score = np.array(0., dtype="float32")
-        for _ in range(self._frame_skip - 1):
+        for t in range(self._frame_skip):
             game_score += self.ale.act(a)
-        self._get_screen(1)
-        game_score += self.ale.act(a)
-        lost_life = self._check_life()  # Advances from lost_life state.
-        if lost_life and self._episodic_lives:
-            self._reset_obs()  # Internal reset.
+            lost_life = self._check_life()  # Advances from lost_life state.
+            if lost_life and self._episodic_lives:
+                self._reset_obs()  # Internal reset.
+                break
+            game_over = self.ale.game_over() or self._step_counter >= self.horizon
+            done = game_over or (self._episodic_lives and lost_life)
+            if t == self.frame_skip - 2:
+                self._get_screen(1)
+            elif t == self.frame_skip - 1:
+                self._get_screen(2)
         self._update_obs(action)
         reward = np.sign(game_score) if self._clip_reward else game_score
-        game_over = self.ale.game_over() or self._step_counter >= self.horizon
-        done = game_over or (self._episodic_lives and lost_life)
         info = EnvInfo(game_score=game_score, traj_done=game_over)
         self._step_counter += 1
         return EnvStep(self.get_obs(), reward, done, info)
@@ -175,7 +178,6 @@ class AtariEnv(Env):
 
     def _update_obs(self, action):
         """Max of last two frames; crop two rows; downsample by 2x."""
-        self._get_screen(2)
         np.maximum(self._raw_frame_1, self._raw_frame_2, self._max_frame)
         img = cv2.resize(self._max_frame, (self.imagesize, self.imagesize), cv2.INTER_LINEAR)
         if len(img.shape) == 2:
