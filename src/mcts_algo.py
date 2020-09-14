1e4faa15a78f430205ea2cea9586c6bd56a1b985
ankeshanand <ankeshanand1994@gmail.com>
2020-09-14 15:19:07 -0400

    Add components to mcts_models and mcts_algo

--- src/mcts_algo.py
+++ src/mcts_algo.py
@@ -3,6 +3,65 @@ import torch
 
 
 class ValueLearning(DQN):
+    def loss(self, samples):
+        """
+        Computes the Distributional Q-learning loss, based on projecting the
+        discounted rewards + target Q-distribution into the current Q-domain,
+        with cross-entropy loss.
+        Returns loss and KL-divergence-errors for use in prioritization.
+        """
+        if self.model.noisy:
+            self.model.head.reset_noise()
+        # start = time.time()
+        log_pred_ps, pred_rew, mpr_loss \
+            = self.agent(samples.all_observation.to(self.agent.device),
+                         samples.all_action.to(self.agent.device),
+                         samples.all_reward.to(self.agent.device),
+                         train=True)  # [B,A,P]
+        rl_loss, KL = self.rl_loss(log_pred_ps[0], samples, 0)
+        if len(pred_rew) > 0:
+            pred_rew = torch.stack(pred_rew, 0)
+            with torch.no_grad():
+                reward_target = to_categorical(samples.all_reward[:self.jumps+1].flatten().to(self.agent.device), limit=1).view(*pred_rew.shape)
+            reward_loss = -torch.sum(reward_target * pred_rew, 2).mean(0).cpu()
+        else:
+            reward_loss = torch.zeros(samples.all_observation.shape[1],)
+        model_rl_loss = torch.zeros_like(reward_loss)
+
+        if self.model_rl_weight > 0:
+            for i in range(1, self.jumps+1):
+                jump_rl_loss, model_KL = self.rl_loss(log_pred_ps[i],
+                                                      samples,
+                                                      i)
+                model_rl_loss = model_rl_loss + jump_rl_loss
+        nonterminals = 1. - torch.sign(torch.cumsum(samples.done.to(self.agent.device), 0)).float()
+        nonterminals = nonterminals[self.model.time_offset:
+                                    self.jumps + self.model.time_offset+1]
+        mpr_loss = mpr_loss*nonterminals
+        if self.jumps > 0:
+            model_mpr_loss = mpr_loss[1:].mean(0)
+            mpr_loss = mpr_loss[0]
+        else:
+            mpr_loss = mpr_loss[0]
+            model_mpr_loss = torch.zeros_like(mpr_loss)
+        mpr_loss = mpr_loss.cpu()
+        model_mpr_loss = model_mpr_loss.cpu()
+        reward_loss = reward_loss.cpu()
+
+        if self.prioritized_replay:
+            weights = samples.is_weights
+            mpr_loss = mpr_loss * weights
+            model_mpr_loss = model_mpr_loss * weights
+            reward_loss = reward_loss * weights
+            # RL losses are no longer scaled in the c51 function
+            rl_loss = rl_loss * weights
+            model_rl_loss = model_rl_loss * weights
+        return rl_loss.mean(), KL, \
+               model_rl_loss.mean(), \
+               reward_loss.mean(), \
+               mpr_loss.mean(), \
+               model_mpr_loss.mean(),
+
     def rl_loss(self, samples):
         """
         Computes value loss for all timesteps in t = [0..jumps)
@@ -23,15 +82,6 @@ class ValueLearning(DQN):
         td_abs_errors = abs_delta.detach()
         if self.delta_clip is not None:
             td_abs_errors = torch.clamp(td_abs_errors, 0, self.delta_clip)
-        if not self.mid_batch_reset:
-            # FIXME: I think this is wrong, because the first "done" sample
-            # is valid, but here there is no [T] dim, so there's no way to
-            # know if a "done" sample is the first "done" in the sequence.
-            raise NotImplementedError
-            # valid = valid_from_done(samples.done)
-            # loss = valid_mean(losses, valid)
-            # td_abs_errors *= valid
-        else:
-            value_loss = torch.mean(value_losses)
-        return value_loss
+        value_loss = torch.mean(value_losses)
+        return value_loss, td_abs_errors
 
---
+++ src/mcts_models.py
@@ -0,0 +1,241 @@
+import torch
+import numpy as np
+import torch.nn as nn
+
+from rlpyt.models.mlp import MlpModel
+from src.rlpyt_models import Conv2dModel
+
+
+class MCTSModel(torch.nn.Module):
+    def __init__(self, image_shape, output_size, jumps, mpr, imagesize,
+                 augmentation, target_augmentation, eval_augmentation,
+                 dynamics_blocks, norm_type, dqn_hidden_size, model_rl,
+                 momentum_tau, renormalize, dropout, residual_tm):
+        f, c = image_shape[:2]
+        in_channels = np.prod(image_shape[:2])
+        self.conv = Conv2dModel(
+            in_channels=in_channels,
+            channels=[32, 64, 64],
+            kernel_sizes=[8, 4, 3],
+            strides=[4, 2, 1],
+            paddings=[0, 0, 0],
+            use_maxpool=False,
+            dropout=dropout,
+        )
+
+        fake_input = torch.zeros(1, f*c, imagesize, imagesize)
+        fake_output = self.conv(fake_input)
+        self.hidden_size = fake_output.shape[1]
+        self.pixels = fake_output.shape[-1]*fake_output.shape[-2]
+        print("Spatial latent size is {}".format(fake_output.shape[1:]))
+
+        self.jumps = jumps
+        self.model_rl = model_rl
+        self.use_mpr = mpr
+        self.target_augmentation = target_augmentation
+        self.eval_augmentation = eval_augmentation
+        self.num_actions = output_size
+
+        self.head = MLPHead(input_channels=self.hidden_size, pixels=self.pixels)
+
+        if self.jumps > 0:
+            self.dynamics_model = TransitionModel(channels=self.hidden_size,
+                                                  num_actions=output_size,
+                                                  pixels=self.pixels,
+                                                  hidden_size=self.hidden_size,
+                                                  limit=1,
+                                                  blocks=dynamics_blocks,
+                                                  norm_type=norm_type,
+                                                  renormalize=renormalize,
+                                                  residual=residual_tm)
+        else:
+            self.dynamics_model = nn.Identity()
+
+        self.renormalize = renormalize
+
+
+class MLPHead(torch.nn.Module):
+    def __init__(self,
+                 input_channels,
+                 output_size=1,
+                 hidden_size=256,
+                 pixels=30,
+                 noisy=0):
+        super().__init__()
+        if noisy:
+            linear = NoisyLinear
+        else:
+            linear = nn.Linear
+        self.noisy = noisy
+        self.linears = [linear(input_channels*pixels, hidden_size),
+                        linear(hidden_size, output_size)]
+        layers = [nn.Flatten(-3, -1),
+                  self.linears[0],
+                  nn.ReLU(),
+                  self.linears[1]]
+        self.network = nn.Sequential(*layers)
+        self._output_size = output_size
+
+    def forward(self, input):
+        return self.network(input)
+
+    def reset_noise(self):
+        for module in self.linears:
+            module.reset_noise()
+
+    def set_sampling(self, sampling):
+        for module in self.linears:
+            module.sampling = sampling
+
+
+class TransitionModel(nn.Module):
+    def __init__(self,
+                 channels,
+                 num_actions,
+                 args=None,
+                 blocks=16,
+                 hidden_size=256,
+                 pixels=36,
+                 limit=300,
+                 action_dim=6,
+                 norm_type="bn",
+                 renormalize=True,
+                 residual=False):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_actions = num_actions
+        self.args = args
+        self.renormalize = renormalize
+        self.residual = residual
+        layers = [Conv2dSame(channels+num_actions, hidden_size, 3),
+                  nn.ReLU(),
+                  init_normalization(hidden_size, norm_type)]
+        for _ in range(blocks):
+            layers.append(ResidualBlock(hidden_size,
+                                        hidden_size,
+                                        norm_type))
+        layers.extend([Conv2dSame(hidden_size, channels, 3)])
+
+        self.action_embedding = nn.Embedding(num_actions, pixels*action_dim)
+
+        self.network = nn.Sequential(*layers)
+        self.reward_predictor = RewardPredictor(channels,
+                                                pixels=pixels,
+                                                limit=limit,
+                                                norm_type=norm_type)
+        self.train()
+
+    def forward(self, x, action):
+        batch_range = torch.arange(action.shape[0], device=action.device)
+        action_onehot = torch.zeros(action.shape[0],
+                                    self.num_actions,
+                                    x.shape[-2],
+                                    x.shape[-1],
+                                    device=action.device)
+        action_onehot[batch_range, action, :, :] = 1
+        stacked_image = torch.cat([x, action_onehot], 1)
+        next_state = self.network(stacked_image)
+        if self.residual:
+            next_state = next_state + x
+        next_state = F.relu(next_state)
+        if self.renormalize:
+            next_state = renormalize(next_state, 1)
+        next_reward = self.reward_predictor(next_state)
+        return next_state, next_reward
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 norm_type="bn"):
+        super().__init__()
+        self.block = nn.Sequential(
+            Conv2dSame(in_channels, out_channels, 3),
+            nn.ReLU(),
+            init_normalization(out_channels, norm_type),
+            Conv2dSame(out_channels, out_channels, 3),
+            init_normalization(out_channels, norm_type),
+        )
+
+    def forward(self, x):
+        residual = x
+        out = self.block(x)
+        out += residual
+        out = F.relu(out)
+        return out
+
+
+class Conv2dSame(torch.nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 bias=True,
+                 stride=1,
+                 padding_layer=nn.ReflectionPad2d):
+        super().__init__()
+        ka = kernel_size // 2
+        kb = ka - 1 if kernel_size % 2 == 0 else ka
+        self.net = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels, out_channels, kernel_size, bias=bias,
+                            stride=stride, padding=ka)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+def init_normalization(channels, type="bn", affine=True, one_d=False):
+    assert type in ["bn", "ln", "in", "none", None]
+    if type == "bn":
+        if one_d:
+            return nn.BatchNorm1d(channels, affine=affine)
+        else:
+            return nn.BatchNorm2d(channels, affine=affine)
+    elif type == "ln":
+        if one_d:
+            return nn.LayerNorm(channels, elementwise_affine=affine)
+        else:
+            return nn.GroupNorm(1, channels, affine=affine)
+    elif type == "in":
+        return nn.GroupNorm(channels, channels, affine=affine)
+    elif type == "none" or type is None:
+        return nn.Identity()
+
+
+class RewardPredictor(nn.Module):
+    def __init__(self,
+                 input_channels,
+                 hidden_size=1,
+                 pixels=36,
+                 limit=300,
+                 norm_type="bn"):
+        super().__init__()
+        self.hidden_size = hidden_size
+        layers = [nn.Conv2d(input_channels, hidden_size, kernel_size=1, stride=1),
+                  nn.ReLU(),
+                  init_normalization(hidden_size, norm_type),
+                  nn.Flatten(-3, -1),
+                  nn.Linear(pixels*hidden_size, 256),
+                  nn.ReLU(),
+                  nn.Linear(256, limit*2 + 1)]
+        self.network = nn.Sequential(*layers)
+        self.train()
+
+    def forward(self, x):
+        return self.network(x)
+
+
+def renormalize(tensor, first_dim=1):
+    if first_dim < 0:
+        first_dim = len(tensor.shape) + first_dim
+    flat_tensor = tensor.view(*tensor.shape[:first_dim], -1)
+    max = torch.max(flat_tensor, first_dim, keepdim=True).values
+    min = torch.min(flat_tensor, first_dim, keepdim=True).values
+    flat_tensor = (flat_tensor - min)/(max - min)
+
+    return flat_tensor.view(*tensor.shape)
+
+
+
